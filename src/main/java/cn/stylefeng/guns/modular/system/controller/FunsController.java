e316488a8c683173c35ad175aab9639723c7644d
yangt <16601047622@163.com>
2019-11-28 17:37:10 +0800

    去除引用

--- src/main/java/cn/stylefeng/guns/modular/system/controller/FunsController.java
+++ src/main/java/cn/stylefeng/guns/modular/system/controller/FunsController.java
@@ -1,23 +1,14 @@
 package cn.stylefeng.guns.modular.system.controller;
 
 import cn.stylefeng.guns.core.common.annotion.Permission;
-import cn.stylefeng.guns.core.common.node.ZTreeNode;
 import cn.stylefeng.guns.core.common.page.LayuiPageFactory;
 import cn.stylefeng.guns.core.util.DESEncrypt;
-import cn.stylefeng.guns.modular.system.service.MarkService;
-import cn.stylefeng.guns.modular.system.warpper.DeptWrapper;
 import cn.stylefeng.roses.core.base.controller.BaseController;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.google.common.collect.Lists;
 import org.apache.logging.log4j.util.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.Map;
 
 
 @Controller
@@ -25,9 +16,9 @@ import java.util.Map;
 public class FunsController extends BaseController {
 
     private static final String PREFIX = "/modular/music/";
-
-    @Autowired
-    private MarkService markService;
+//
+//    @Autowired
+//    private MarkService markService;
 
     @RequestMapping("/music")
     public String music() {
@@ -39,13 +30,13 @@ public class FunsController extends BaseController {
         return PREFIX + "diary.html";
     }
 
-    @RequestMapping("/init/mark")
-    @ResponseBody
-    public List<ZTreeNode> tree() {
-        List<ZTreeNode> tree = markService.tree();
-        tree.add(ZTreeNode.createParent());
-        return tree;
-    }
+//    @RequestMapping("/init/mark")
+//    @ResponseBody
+//    public List<ZTreeNode> tree() {
+//        List<ZTreeNode> tree = markService.tree();
+//        tree.add(ZTreeNode.createParent());
+//        return tree;
+//    }
 
     @RequestMapping("/decrypt")
     @ResponseBody
