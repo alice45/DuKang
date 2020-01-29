45058773a548655347a40f8183abc8adae6e0730
yangt8 <yangt8@ziroom.com>
2020-01-29 17:10:56 +0800

    movie

--- src/main/java/cn/stylefeng/guns/modular/system/controller/FunsController.java
+++ src/main/java/cn/stylefeng/guns/modular/system/controller/FunsController.java
@@ -3,9 +3,11 @@ package cn.stylefeng.guns.modular.system.controller;
 import cn.stylefeng.guns.core.common.annotion.Permission;
 import cn.stylefeng.guns.core.common.page.LayuiPageFactory;
 import cn.stylefeng.guns.core.util.DESEncrypt;
+import cn.stylefeng.guns.core.util.TPUtils;
 import cn.stylefeng.roses.core.base.controller.BaseController;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,9 +21,6 @@ import java.util.Optional;
 public class FunsController extends BaseController {
 
     private static final String PREFIX = "/modular/music/";
-//
-//    @Autowired
-//    private MarkService markService;
 
     @RequestMapping("/music")
     public String music() {
@@ -33,23 +32,12 @@ public class FunsController extends BaseController {
         return PREFIX + "diary.html";
     }
 
-//    @RequestMapping("/init/mark")
-//    @ResponseBody
-//    public List<ZTreeNode> tree() {
-//        List<ZTreeNode> tree = markService.tree();
-//        tree.add(ZTreeNode.createParent());
-//        return tree;
-//    }
 
-//    @RequestMapping("/decrypt")
-//    @ResponseBody
-//    public String decrypt(String content) {
-//        if (Strings.isEmpty(content)) {
-//            return "空的~解密啥哪";
-//        }
-//        return DESEncrypt.decrypt(content);
-//    }
-
+    @RequestMapping("/")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("input.html");
+        return mv;
+    }
 
     @RequestMapping("/decrypt")
     public ModelAndView decrypt() {
@@ -67,13 +55,16 @@ public class FunsController extends BaseController {
     }
 
 
-    @RequestMapping(value = "/list")
-    @Permission
+    @RequestMapping("/tp/{num}")
     @ResponseBody
-    public Object list(@RequestParam(value = "condition", required = false) String condition,
-                       @RequestParam(value = "deptId", required = false) Long deptId) {
-//        Page<Map<String, Object>> list = this.null.list(condition, deptId);
-//        Page<Map<String, Object>> wrap = new DeptWrapper(list).wrap();
-        return LayuiPageFactory.createPageInfo(null);
+    public String decryptKey(@PathVariable(value = "num") Long num) {
+
+        return "";
     }
+
+    public static void main(String[] args) {
+        TPUtils tpUtils = new TPUtils(95d);
+        System.out.println(tpUtils.getResult());
+    }
+
 }
